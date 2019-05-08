5e7eff1c6d343ed4804c278ca648ba3e8be64b7f
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-05-08 12:07:29 +1000

    Added Sprint

---
+++ Assets/Events/OnTriggerEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerEvent : MonoBehaviour
+{
+    // Reference Tag to detect collisions with
+    public string hitTag;
+    public UnityEvent onEnter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If hitting hit Tag
+        if(other.tag == hitTag || hitTag == "")
+        {
+            // Invoke (Run) the event!
+            onEnter.Invoke();
+        }
+    }
+}
